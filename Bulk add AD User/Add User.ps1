a26ff2665a0cf1615effdc1a3d19111e28019134
Your Name <adammhancock@gmail.com>
2018-02-01 18:11:50 +0000

    Added Bulk Add user

---
+++ Bulk add AD User/Add User.ps1
@@ -0,0 +1,7 @@
+﻿## Adam Hancock
+
+$csv = Import-Csv users.csv
+$csv | ForEach-Object {
+$password =  $_.password | ConvertTo-SecureString -AsPlainText -Force
+New-ADUser -SamAccountName $_.username -GivenName $_.Firstname -Surname $_.Surname -DisplayName ($_.Firstname + " " + $_.Surname) -Name ($_.Firstname + " " + $_.Surname) -AccountPassword $password -EmailAddress $_.email -UserPrincipalName $_.email -Enabled $true -title $_.Title
+}
