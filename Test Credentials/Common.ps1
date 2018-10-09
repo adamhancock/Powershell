9c0b73d54d95d561fb38663edeeb776600dd23f3
Adam Hancock <adammhancock@gmail.com>
2018-10-09 22:45:51 +0100

    restructure

--- Test Credentials/Common.ps1
+++ Test Credentials/Common.ps1
@@ -19,22 +19,33 @@ function Test-ADCredential {
         $result = $DS.ValidateCredentials($UserName, $Password)
         if ($result) {
             write-host -ForegroundColor Red Password: $Password - $UserName
+            Write-Output $Password - $UserName | Out-File users.txt -Append
         }
     }
 }
 
 Write-Host "Downloading Password List..."
 Invoke-WebRequest -Uri "https://ingeniotech.blob.core.windows.net/download/passwordlist.txt" -OutFile passwords.txt
-
+$total = Get-Content passwords.txt| Measure-Object –Line
 Write-Host "Testing Passwords..."
 
-foreach ($password in Get-Content .\passwords.txt) {
-    if ($password -match $regex) {
-        $users = $null  
-                   
-        Get-ADUser -Filter * -Properties userPrincipalName | foreach { Test-ADCredential -username $_.userPrincipalName -password $password   }
-        $sam = Get-ADUser -Filter {userPrincipalName -eq $username}
-        Unlock-ADAccount -Identity $sam
+foreach ($user in Get-ADUser -Filter * -Properties Name, userPrincipalName) {
+    write-host $user.Name -ForegroundColor Green
+    $i = 1;
+    foreach ($password in Get-Content .\passwords.txt) {
+     
+        write-host $i '/' $total.Lines
+        # write-host $user.userPrincipalName - $user.LockedOut   
+        Test-ADCredential -username $user.userPrincipalName  -password $password 
+            
+        $lockedout = Get-ADUser -identity $user -Properties LockedOut
+        # write-host $lockedout.LockedOut
+        if ($lockedout.LockedOut) {
+            # write-host "Unlocking.."
+            Unlock-ADAccount -Identity $user.SamAccountName
+        }
+        $i++
 
     }
+
 }
