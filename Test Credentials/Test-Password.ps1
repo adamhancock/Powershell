48b510ab2142b21cac3e279027d41ccf6874e519
Adam Hancock <adam@ingeniotech.co.uk>
2018-10-09 13:00:41 +0100

    test password

---
+++ Test Credentials/Test-Password.ps1
@@ -0,0 +1,30 @@
+# Adam Hancock
+
+Import-Module ActiveDirectory  
+
+$users = $null  
+
+function Test-ADCredential {
+    [CmdletBinding()]
+    Param
+    (
+        [string]$UserName,
+        [string]$Password
+    )
+    if (!($UserName) -or !($Password)) {
+       # Write-Warning 'Test-ADCredential: Please specify both user name and password'
+    } else {
+    
+        Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+        $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('domain')
+       $result = $DS.ValidateCredentials($UserName, $Password)
+       if ($result){
+       write-host $UserName
+       }
+    }
+}
+
+#####################################
+
+
+Get-ADUser -Filter * -Properties userPrincipalName | foreach { Test-ADCredential -username $_.userPrincipalName -password Password123   }
